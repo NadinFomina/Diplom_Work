5d099a4d2b21e405c7db1f2635a3835d3e91a6a0
NadinFomina <8241021nf@mail.ru>
2023-02-15 18:50:43 +0300

    tic tak toe

--- game/src/component/Game.js
+++ game/src/component/Game.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { useSpring, animated } from '@react-spring/web';
 import { useState } from "react";
 import '../style/game.css'
 import "../style/squre.css"
 import Board from "./Board";
 import { calculateWinner } from "../helper";
+
 const Game = () => {
     const [board, setBoard] = useState(Array(9).fill(null))// доска с 9ю ячейками
     const [xIsNext, setXisNext] = useState(true) // массив кто ходит
     const winner = calculateWinner(board);
     const handelClick = (index) => {
         const newBoard = [...board]
-        // Определение был ли клик по  ячейке или игра закончилась
+               // Определение был ли клик по  ячейке или игра закончилась
         if (winner || newBoard[index]) return
         // определить чей ход Х?О
-        newBoard[index] = xIsNext ? 'X' : 'O'
+        newBoard[index] = xIsNext ? 'X' : '0'
         // обновить состояние
         setBoard(newBoard)
         setXisNext(!xIsNext)
@@ -21,13 +23,13 @@ const Game = () => {
     }
     const startNewGame = () => {
         return (
-            <button className="start_btn" onClick={()=>setBoard(Array(9).fill(null))}> Начать заново</button>
+            <button className="start_btn" onClick={() => setBoard(Array(9).fill(null))}> Начать заново</button>
         )
     }
 
     return (
         <div className="game">
-            <h3 className="text_game">{winner ? "Выйграл игрок -" + " " + winner : "Сейчас ход -" + " " + (xIsNext ? "X" : "O" ) }</h3>
+            <h3 className="text_game">{winner ? "Выйграл игрок - " + " " + winner : "Сейчас ход -" + " " + (xIsNext ? "X" : "O")}</h3>
             <Board squres={board} style={xIsNext ? "isActiveX" : " "} click={handelClick} />
             {startNewGame()}
         </div>
